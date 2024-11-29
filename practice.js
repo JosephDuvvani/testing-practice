a5d13c72cc3f7676d58e3525788c1e476b33c947
JosephDuvvani <josephduvvani@gmail.com>
2024-11-29 20:56:03 +0200

    Create the reverseString function

--- practice.js
+++ practice.js
@@ -3,3 +3,13 @@ export const capitalise = (string) => {
   cap[0] = cap[0].toUpperCase();
   return cap.join("");
 };
+
+export const reverseString = (string) => {
+  let reversed = [];
+  const charArray = string.split("");
+  const lastIndex = charArray.length - 1;
+  for (let i = lastIndex; i >= 0; i--) {
+    reversed.push(charArray[i]);
+  }
+  return reversed.join("");
+};

63682edfa0d5624f645cc3256c4150152f73d6ff
JosephDuvvani <josephduvvani@gmail.com>
2024-11-29 20:45:03 +0200

    Fix: change tobe to toBe
    B is uppercase

--- practice.test.js
+++ practice.test.js
@@ -16,39 +16,39 @@ test("Capitalised 'paige' is 'Paige'", () => {
 
 //reverseString
 test("Reversed 'Hello' is 'olleH'", () => {
-  expect(reverseString("Hello")).tobe("olleH");
+  expect(reverseString("Hello")).toBe("olleH");
 });
 test("Reversed 'table' is 'elbat'", () => {
-  expect(reverseString("table")).tobe("elbat");
+  expect(reverseString("table")).toBe("elbat");
 });
 
 //calculator
 test("1 + 1 equals 2", () => {
-  expect(calculator.add(1, 1)).tobe(2);
+  expect(calculator.add(1, 1)).toBe(2);
 });
 test("234 + 531 equals 765", () => {
-  expect(calculator.add(234, 531)).tobe(765);
+  expect(calculator.add(234, 531)).toBe(765);
 });
 
 test("1 - 1 equals 0", () => {
-  expect(calculator.subtract(1, 1)).tobe(0);
+  expect(calculator.subtract(1, 1)).toBe(0);
 });
 test("25 - 4 equals 21", () => {
-  expect(calculator.subtract(25, 4)).tobe(21);
+  expect(calculator.subtract(25, 4)).toBe(21);
 });
 
 test("2 / 2 equals 1", () => {
-  expect(calculator.add(2, 2)).tobe(1);
+  expect(calculator.add(2, 2)).toBe(1);
 });
 test("9 / 3 equals 3", () => {
-  expect(calculator.add(9, 3)).tobe(3);
+  expect(calculator.add(9, 3)).toBe(3);
 });
 
 test("2 * 2 equals 4", () => {
-  expect(calculator.add(2, 2)).tobe(4);
+  expect(calculator.add(2, 2)).toBe(4);
 });
 test("5 * 8 equals 40", () => {
-  expect(calculator.add(5, 8)).tobe(40);
+  expect(calculator.add(5, 8)).toBe(40);
 });
 
 //caesar cipher
